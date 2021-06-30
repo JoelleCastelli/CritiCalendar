3ffb528763e564c56d280b77d24e1af71489add8
Joelle <castelli.jo@gmail.com>
2021-06-30 11:11:03 +0200

    add owner and theme functions to campaign model

--- app/Models/Campaign.php
+++ app/Models/Campaign.php
@@ -9,5 +9,15 @@ class Campaign extends Model
 {
     use HasFactory;
 
+    /**
+     * Get the user that owns the campaign (i.e. GM)
+     */
+    public function owner() {
+        return $this->belongsTo(User::class, 'master_id');
+    }
+
+    public function theme() {
+        return $this->belongsTo(Theme::class, 'theme_id');
+    }
 
 }
