2f37f2b8b4125a302fae958a59e060589c699b83
Suvirtha <suvirtha.thaya@finelia.fr>
2021-07-20 14:54:41 +0200

    fix merge

--- app/Http/Livewire/Calendar.php
+++ app/Http/Livewire/Calendar.php
@@ -3,11 +3,15 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\Sessions;
 
 class Calendar extends Component
 {
+    public $events = [];
+
     public function render()
     {
+        $this->events = json_encode(Sessions::all());
         return view('livewire.calendar');
     }
 }
