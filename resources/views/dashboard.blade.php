20cb60c41a71a17beb7509dd53c008c66793c5b9
Joelle <castelli.jo@gmail.com>
2021-07-22 17:46:55 +0200

    fix next campaign

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,7 +34,7 @@
                         <p>
                             <span class="font-weight-bold">Titre : </span> {{ $data['nextSession']->title }}<br>
                             <span class="font-weight-bold">Date : </span> {{  Carbon\Carbon::createFromTimeString($data['nextSession']->start)->format('d/m/Y H:i') }}<br>
-                            <span class="font-weight-bold">Titre de la campagne : </span> {{ $data['nextSession']->campaign->name }}<br>
+                            <span class="font-weight-bold">Campagne : </span> {{ $data['nextSession']->campaign->name }}<br>
                         </p>
                     </div>
                     <div class="my-2">
