fc34c81c33c8b36294e554d1a18ee485c98717b2
Coraline <cesedji@myges.fr>
2021-07-22 16:14:23 +0200

    add session display to dashboard

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Event extends Model
 {
     use HasFactory;
+
+    public function campaign()
+    {
+        return $this->belongsTo(Campaign::class, 'campaign_id');
+    }
 }
