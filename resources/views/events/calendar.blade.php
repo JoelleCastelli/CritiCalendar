ed19728d17e405006734b5b4ad6cb622b5dcc2ec
Joelle <castelli.jo@gmail.com>
2021-07-27 18:05:30 +0200

    Calendar: remove title

--- resources/views/events/calendar.blade.php
+++ resources/views/events/calendar.blade.php
@@ -5,25 +5,16 @@
 </h2>
 </x-slot>
 
-    @if (session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+@include('components.flash')
 
 <div class="py-12">
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-
             <div class="p-6 bg-white border-b border-gray-200">
-
-                <h2> Calendrier des sessions </h2>
                 <script src="{{asset('vendor/livewire/livewire.js')}}"></script>
                 <livewire:calendar />
 
                 @stack('scripts')
-
-
             </div>
         </div>
     </div>
